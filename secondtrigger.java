5866433ddc77918d2174990eb9267008fe670089
Abhipowar2410 <abhipowar100@gmail.com>
2024-07-31 12:05:11 +0530

    Second trigger java

---
+++ secondtrigger.java
@@ -0,0 +1,12 @@
+public class simple{
+public static void main(String args[]){
+
+int a=50;
+int b=90;
+
+System.out.println("First No is:"(a+b));
+System.out.println("Second No is:"(a-b));
+System.out.println("Addition is:"+(a+b));
+
+}
+}
